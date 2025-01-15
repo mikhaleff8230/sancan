3dfbace89b3d49460d7fb782134eb540d0e37d20
mikhaleff8230 <100951143+mikhaleff8230@users.noreply.github.com>
2025-01-15 21:30:31 +0300

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://sancan.ru',
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 1000,
+    generateRobotsTxt: true,
+    
+  }
